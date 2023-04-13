fc49b453dcd691e1aa54709fc2d86ad7d2f0f74d
ndeepakreddy <126979353+ndeepakreddy@users.noreply.github.com>
2023-04-13 19:04:46 +0530

    Add files via upload

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const todoList = () => {
   all = []
   const add = (todoItem) => {
@@ -47,48 +48,4 @@ const todoList = () => {
     toDisplayableList
   }
 }
-
-// ####################################### #
-// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-// ####################################### #
-
-const todos = todoList()
-
-const formattedDate = d => {
-  return d.toISOString().split('T')[0]
-}
-
-const dateToday = new Date()
-const today = formattedDate(dateToday)
-const yesterday = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() - 1))
-)
-const tomorrow = formattedDate(
-  new Date(new Date().setDate(dateToday.getDate() + 1))
-)
-
-todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-
-console.log('My Todo-list\n')
-
-console.log('Overdue')
-const overdues = todos.overdue()
-const formattedOverdues = todos.toDisplayableList(overdues)
-console.log(formattedOverdues)
-console.log('\n')
-
-console.log('Due Today')
-const itemsDueToday = todos.dueToday()
-const formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-console.log(formattedItemsDueToday)
-console.log('\n')
-
-console.log('Due Later')
-const itemsDueLater = todos.dueLater()
-const formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-console.log(formattedItemsDueLater)
-console.log('\n\n')
+module.exports = todoList
